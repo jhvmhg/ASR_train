bea70b888720227ef40c94630e31ac565aba13ac
Mcy <1192554423@qq.com>
2020-05-14 16:18:10 +0800

    数据增强脚本第一次提交

---
+++ audio_aug/local/dump_file_multi.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+nj=40
+cmd=run.pl
+
+
+
+echo "$0 $@"  # Print the command line for logging.
+
+if [ -f path.sh ]; then . ./path.sh; fi
+. parse_options.sh || exit 1;
+
+
+srcdir=$1
+scp=${srcdir}/wav.scp
+
+if [ $# -ge 2 ]; then
+  logdir=$2
+else
+  logdir=${srcdir}/log
+fi
+
+mkdir -p $logdir || exit 1;
+mkdir -p ${srcdir}/wav || exit 1;
+
+name=`basename $srcdir`
+# End configuration section.
+
+split_scps=
+for n in $(seq $nj); do
+  split_scps="$split_scps $logdir/wav_${name}.$n.scp"
+done
+
+
+utils/split_scp.pl $scp $split_scps || exit 1;
+
+
+for n in $(seq $nj); do
+    cat $logdir/wav_${name}.$n.scp | local/dump_wavs.py ${srcdir} >/dev/null 2>&1 &
+done
+
+echo "Wait for a few minutes, the new wavs are dumping to disk!"
+
