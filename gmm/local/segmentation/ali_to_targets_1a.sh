9b75821ab9e743c4c3247c0ff3adf3a0bbac9d29
meichaoyang <meichaoyang@yiwise.com>
2020-07-20 11:22:45 +0800

    Add 添加使用kaldiio生成targets的脚本

---
+++ gmm/local/segmentation/ali_to_targets_1a.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+# Copyright 2017  Vimal Manohar
+# Apache 2.0
+
+# This script converts alignments into targets for training neural network
+# for speech activity detection. The mapping from phones to speech / silence / garbage
+# is defined by the options --silence-phones and --garbage-phones.
+# This is similar to the script steps/segmentation/lats_to_targets.sh which
+# converts lattices to targets. See that script for details about the
+# targets matrix.
+
+set -o pipefail
+
+silence_phones=
+max_phone_duration=0.5
+
+cmd=run.pl
+
+[ -f ./path.sh ] && . ./path.sh
+. utils/parse_options.sh
+
+if [ $# -ne 4 ]; then
+  cat <<EOF
+  This script converts alignments into targets for training neural network
+  for speech activity detection. The mapping from phones to speech / silence / garbage
+  is defined by the options --silence-phones and --garbage-phones.
+
+  This is similar to the script steps/segmentation/lats_to_targets.sh which
+  converts lattices to targets. See that script for details about the
+  targets matrix.
+
+  Usage: local/segmentation/ali_to_targets.sh <data-dir> <lang> <ali-dir> <targets-dir>"
+  e.g.: local/segmentation/ali_to_targets.sh \
+  --silence-phones data/lang/phones/optional_silence.txt \
+  --garbage-phones data/lang/phones/silence.txt \
+  --max-phone-duration 0.5 \
+  data/train_split10s data/lang \
+  exp/segmentation1a/tri3b_train_split10s_ali \
+  exp/segmentation1a/tri3b_train_split10s_targets
+EOF
+  exit 1
+fi
+
+data=$1
+lang=$2
+ali_dir=$3
+dir=$4
+
+if [ -f $ali_dir/final.mdl ]; then
+  srcdir=$ali_dir
+else
+  srcdir=$ali_dir/..
+fi
+
+for f in $data/utt2spk $ali_dir/ali.1.gz $srcdir/final.mdl; do
+  if [ ! -f $f ]; then
+    echo "$0: Could not find file $f"
+    exit 1
+  fi
+done
+
+mkdir -p $dir
+
+
+if [ -z "$silence_phones" ]; then
+  cat $lang/silence_phones.txt | \
+    utils/filter_scp.pl --exclude $dir/garbage_phones.txt > \
+    $dir/silence_phones.txt
+else
+  cp $silence_phones $dir/silence_phones.txt
+fi
+
+nj=$(cat $ali_dir/num_jobs) || exit 1
+
+$cmd JOB=1:$nj $dir/log/get_phone_ali_info.JOB.log \
+  ali-to-phones --per-frame  \
+    $srcdir/final.mdl "ark:gunzip -c $ali_dir/ali.JOB.gz |" - \| \
+    ark,t:$dir/phone_ali_info.JOB.txt || exit 1
+
+# make $dir an absolute pathname.
+dir=`perl -e '($dir,$pwd)= @ARGV; if($dir!~m:^/:) { $dir = "$pwd/$dir"; } print $dir; ' $dir ${PWD}`
+
+frame_subsampling_factor=1
+if [ -f $srcdir/frame_subsampling_factor ]; then
+  frame_subsampling_factor=$(cat $srcdir/frames_subsampling_factor)
+  echo $frame_subsampling_factor > $dir/frame_subsampling_factor
+fi
+
+frame_shift=$(utils/data/get_frame_shift.sh $data) || exit 1
+max_phone_len=$(perl -e "print int($max_phone_duration / $frame_shift)")
+
+$cmd JOB=1:$nj $dir/log/get_targets.JOB.log \
+  python local/segmentation/phone_info_to_targets.py \
+    --silence-phones=$dir/silence_phones.txt \
+    ark,t:$dir/phone_ali_info.JOB.txt \
+    ark,scp:$dir/targets.JOB.ark,$dir/targets.JOB.scp || exit 1
+
+for n in $(seq $nj); do
+  cat $dir/targets.$n.scp
+done > $dir/targets.scp
+
+steps/segmentation/validate_targets_dir.sh $dir $data || exit 1
+
+echo "$0: Done creating targets in $dir/targets.scp"
+
---
+++ gmm/local/train_seg_1a.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+set -e
+
+nj=24
+
+# configs for 'chain'
+stage=9
+train_stage=-10
+get_egs_stage=-10
+
+
+# training options
+num_epochs=4
+initial_effective_lrate=0.001
+final_effective_lrate=0.0001
+leftmost_questions_truncate=-1
+max_param_change=2.0
+final_layer_normalize_target=0.5
+num_jobs_initial=1
+num_jobs_final=3
+minibatch_size=128
+frames_per_eg=150
+remove_egs=false
+common_egs_dir=
+xent_regularize=0.1
+
+lang=../gmm/data/lang
+feat_dir=../train_fbank_combine
+ali_dir=exp/chain/chain_combine_fake_align_lat
+targets_dir=exp/segmentation1a/train_fbank_combine_targets
+
+# End configuration section.
+echo "$0 $@"  # Print the command line for logging
+
+. ./cmd.sh
+. ./path.sh
+. ./utils/parse_options.sh
+
+
+dir=$1
+
+
+
+if [ $stage -le 11 ]; then
+
+    bash local/segmentation/ali_to_targets_1a.sh \
+      --silence-phones ../gmm/data/lang/phones/silence.txt \
+      --max-phone-duration 0.5 \
+      $feat_dir $lang \
+      $ali_dir $targets_dir
+fi
+
+if [ $stage -le 12 ]; then
+  mkdir -p $dir
+  echo "$0: creating neural net configs using the xconfig parser";
+
+  num_targets=2
+
+  mkdir -p $dir/configs
+  cat <<EOF > $dir/configs/network.xconfig
+  input dim=40 name=input
+
+  # please note that it is important to have input layer with the name=input
+  # as the layer immediately preceding the fixed-affine-layer to enable
+  # the use of short notation for the descriptor
+#   fixed-affine-layer name=lda input=Append(-2,-1,0,1,2,ReplaceIndex(ivector, t, 0)) affine-transform-file=$dir/configs/lda.mat
+
+  # the first splicing is moved before the lda layer, so no splicing here
+  relu-renorm-layer name=tdnn1 dim=650 input=Append(-2,-1,0,1,2)
+  relu-renorm-layer name=tdnn2 dim=650 input=Append(-1,0,1)
+  relu-renorm-layer name=tdnn3 dim=650 input=Append(-1,0,1)
+  relu-renorm-layer name=tdnn4 dim=650 input=Append(-3,0,3)
+  relu-renorm-layer name=tdnn5 dim=650 input=Append(-6,-3,0)
+  output-layer name=output dim=$num_targets max-change=1.5
+EOF
+  steps/nnet3/xconfig_to_configs.py --xconfig-file $dir/configs/network.xconfig --config-dir $dir/configs/
+fi
+
+
+if [ $stage -le 13 ]; then
+  steps/nnet3/train_raw_dnn.py --stage $train_stage \
+    --cmd "$decode_cmd" \
+    --use-gpu wait \
+    --feat.cmvn-opts "--norm-means=false --norm-vars=false" \
+    --targets-scp exp/segmentation1a/train_fbank_combine_targets/targets.scp \
+    --egs.dir "$common_egs_dir" \
+    --egs.stage $get_egs_stage \
+    --trainer.num-epochs $num_epochs \
+    --trainer.optimization.num-jobs-initial $num_jobs_initial \
+    --trainer.optimization.num-jobs-final $num_jobs_final \
+    --trainer.optimization.initial-effective-lrate $initial_effective_lrate \
+    --trainer.optimization.final-effective-lrate $final_effective_lrate \
+    --trainer.max-param-change $max_param_change \
+    --cleanup.remove-egs $remove_egs \
+    --feat-dir $feat_dir \
+    --dir $dir  || exit 1;
+
+fi
