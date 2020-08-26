be83526d20c0e630bb740f5122b57a43fcc590d2
meichaoyang <meichaoyang@yiwise.com>
2020-08-26 19:17:51 +0800

    Fix nj

--- ext_feats/ext_mfcc.sh
+++ ext_feats/ext_mfcc.sh
@@ -13,5 +13,5 @@ if [ ! -f $data_path/wav.scp ];then
     exit 1
 fi
 
-steps/make_mfcc.sh --nj $n --cmd "$train_cmd" $data_path $data_path/log $data_path/_mfcc || exit 1;
+steps/make_mfcc.sh --nj $nj --cmd "$train_cmd" $data_path $data_path/log $data_path/_mfcc || exit 1;
 steps/compute_cmvn_stats.sh $data_path $data_path/cmvn_log $data_path/_cmvn || exit 1;
